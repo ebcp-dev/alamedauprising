72c1b527f053aabce8173ad3c3e58b9001776a6a
ebcp.dev <ebcp.dev@gmail.com>
2017-07-09 23:50:14 -0700

    smooth scrolling added

--- public/js/js/animations.js
+++
@@ -1,7 +0,0 @@
-//dependencies
-const polyfill = require('smoothscroll-polyfill').polyfill()
-
-function smoothscroll() {
-    var link = document.getElementsByClassName("nav-item")
-    document.querySelector('.hello').scrollIntoView({ behavior: 'smooth' });
-}
