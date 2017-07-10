9ef23c25717d9336b11e8d7992823d403feea85c
ebcp.dev <ebcp.dev@gmail.com>
2017-07-09 19:28:58 -0700

    added content

--- routes/index.js
+++ routes/index.js
@@ -4,34 +4,72 @@ const router = express.Router()
 
 let roster = [
     {
-        position: 'Guard',
-        name: 'player1',
-        height: '5\'9',
-        age: 16
+        name: 'Quentel Reed',
+        jersey: 00
     },
     {
-        position: 'Guard',
-        name: 'player2',
-        height: '5\'11',
-        age: 15
+        name: 'Cyrus Lavassani',
+        jersey: 9
     },
     {
-        position: 'Forward',
-        name: 'player3',
-        height: '6\'3',
-        age: 17
+        name: 'Landis Green',
+        jersey: 1
     },
     {
-        position: 'Center',
-        name: 'player4',
-        height: '6\'7',
-        age: 17
+        name: 'Dylan Ravan',
+        jersey: 0
+    },
+    {
+        name: 'William Mustain',
+        jersey: 5
+    },
+    {
+        name: 'Nick Saikley',
+        jersey: 6
+    },
+    {
+        name: 'Desmond Ho',
+        jersey: 2
+    },
+    {
+        name: 'Kobe Kiener',
+        jersey: 7
+    },
+    {
+        name: 'Lamonte Mustain',
+        jersey: 11
+    },
+    {
+        name: 'Robert Holt',
+        jersey: 3
+    },
+    {
+        name: 'Darius Hayden',
+        jersey: 22
+    },
+    {
+        name: 'Rodrigo De Souza',
+        jersey: 24
+    },
+    {
+        name: 'Elijah',
+        jersey: 8
     }
+]
+
+let pics = [
+    "coketourny1",
+    "coketourny2",
+    "coketourny3",
+    "coketourny4",
+    "coketourny5",
+    "coketourny6",
+    "coketourny7",
 ]
 
 //get homepage
 router.get('/', function(req, res) {
-    res.render('index', {roster})
+    res.render('index', {roster, pics})
 })
 
 module.exports = router
